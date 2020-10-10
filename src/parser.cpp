5e38cd71cd1f753510e4c884c69e59fa0dd5e56a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-10 15:46:21 -0300

    fixing include guards

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,7 +1,7 @@
-#include "includes/parser.h"
-#include "string.h"
-#include "stdio.h"
-#include "fstream"
+#include "parser.h"
+#include <string.h>
+#include <stdio.h>
+#include <fstream>
 
 static const char LEFT_BRACKET = '<';
 static const char RIGHT_BRACKET = '>';
@@ -12,7 +12,7 @@ Parser::Parser(std::ifstream &file) : file_(file) {}
 bool Parser::parse_file() {
   std::string line;
   std::string tag;
-  std::size_t tag_begin; 
+  std::size_t tag_begin;
   std::size_t tag_length;
   while (getline(file_, line)) {
     for (size_t i = 0; i < line.size(); i++) {
@@ -20,12 +20,12 @@ bool Parser::parse_file() {
         tag_begin = i;
       }
 
-      if (line[i] == RIGHT_BRACKET) { 
+      if (line[i] == RIGHT_BRACKET) {
         tag_length = i + 1 - tag_begin;
         tag = line.substr(tag_begin, tag_length);
 
         printf("%s", tag.c_str());
-        if (tag[1] == SLASH) { 
+        if (tag[1] == SLASH) {
           std::string last_tag = linked_list.pop_back();
 
 
@@ -45,4 +45,5 @@ bool Parser::parse_file() {
 
 bool Parser::match(std::string opening_tag, std::string closing_tag) {
   // retrive the content of each tag and compare
+  return true; // Avoiding compiler warning
 }
