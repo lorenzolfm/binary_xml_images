7c9f22b033ecc772485b70e58de153377ecc562f
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-11 14:34:11 -0300

    moving main.cpp from src dir to address multiple main functions for build issue

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ static char BASE_PATH[] = "assets/";
 
 int main (int argc, char *argv[]) {
   char * file_name = argv[1];
-  char * full_path = strcat(BASE_PATH, file_name); 
+  char * full_path = strcat(BASE_PATH, file_name);
   ifstream file(full_path);
 
-  if(!file.is_open()) { 
+  if(!file.is_open()) {
     printf("ERROR: Unable to open file %s", full_path);
     exit(1);
   }
