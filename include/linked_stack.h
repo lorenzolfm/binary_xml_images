97be12a1dfafcd5894e6cd00928e15fd7f5a91bd
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-22 14:40:52 -0300

    using linked_stack as parser data structure

--- include/linked_stack.h
+++ include/linked_stack.h
@@ -2,111 +2,171 @@
 #ifndef STRUCTURES_LINKED_LIST_H
 #define STRUCTURES_LINKED_LIST_H
 #include <stdio.h>
+
 #include <cstdint>
 #include <stdexcept>
 
-
 namespace structures {
 
-//! ...
-template<typename T>
-class LinkedList {
+template <typename T>
+class LinkedStack {
  public:
-    //! ...
-    LinkedList();  // construtor padrão
-    //! ...
-    ~LinkedList();  // destrutor
-    //! ...
-    void clear();  // limpar
-    //! ...
-    void push_back(const T& data);  // inserir no fim
-    //! ...
-    void push_front(const T& data);  // inserir no início
-    //! ...
-    void insert(const T& data, std::size_t index);  // inserir na posição
-    //! ...
-    void insert_sorted(const T& data);  // inserir em ordem
-    //! ...
-    T& at(std::size_t index);  // acessar um elemento na posição index
-    //! ...
-    T pop(std::size_t index);  // retirar da posição
-    //! ...
-    T pop_back();  // retirar do fim
-    //! ...
-    T pop_front();  // retirar do início
-    //! ...
-    void remove(const T& data);  // remover específico
-    //! ...
-    bool empty() const;  // lista vazia
-    //! ...
-    bool contains(const T& data) const;  // contém
-    //! ...
-    std::size_t find(const T& data) const;  // posição do dado
-    //! ...
-    std::size_t size() const;  // tamanho da lista
+  //! Construtor
+  /*!
+     Chamado na inicialização do objeto.
+  */
+  LinkedStack(void);
+
+  //! Destrutor
+  /*!
+     Destrói objeto quando esse sai de contexto.
+  */
+  ~LinkedStack(void);
+
+  //! Limpa Pilha
+  /*!
+     Método Limpa Pilha. Remove todos os elementos da pilha.
+  */
+  void clear(void);
+
+  //! Empilha Dado
+  /*!
+     Método Empilha Elemento. Recebe uma referência constante ao dado a ser
+     empilhado e adiciona um nodo contendo o dado no topo da pilha.
+     \param data: Referência constante ao dado a ser empilhado (const T&).
+  */
+  void push(const T& data);
+
+  //! Desempilha Dado
+  /*!
+     Método Desempilha Dado. Desempilha o nodo no topo da pilha e retorna o dado
+     de tipo genérico (T) que contém.
+     \return Dado contido no nodo que foi desempilhado, de tipo genérico (T).
+   */
+  T pop(void);
+
+  //! Topo da Pilha.
+  /*!
+     Método Topo da Pilha. Retorna uma referência ao dado de tipo genérico no
+     topo da pilha. Pode ser usado como operador LHS.
+     \return Referência ao dado de tipo genérico (T) contido no nodo no topo da
+     estrutura.
+   */
+  T& top(void);
+
+  //! Topo da Pilha (constante).
+  /*!
+     Método constante topo da pilha. Retorna uma referência ao dado de tipo
+     genérico no topo da pilha. É um método constante, ou seja, não pode alterar
+     o objeto que o chamou (enforçado pelo compilador).
+     \return Referếncia ao dado de tipo genérico (T) contido no nodo do topo da
+     pilha.
+   */
+  const T& top(void) const;
+
+  //! Pilha Vazia
+  /*!
+     Método constante Pilha Vazia. Retorna um valor booleano, verdadeiro se a
+     pilha está vazia, falso se a pilha não está vazia.
+     \return true: Pilha vazia.
+     \return false: Pilha não vazia.
+   */
+  bool empty(void) const;
+
+  //! Tamanho da Pilha
+  /*!
+     Método constante Tamanho da Pilha. Retorna o tamanho atual da pilha.
+     \return Tamanho atual da pilha (size_t).
+   */
+  std::size_t size(void) const;
 
  private:
-    class Node {  // Elemento
-     public:
-        explicit Node(const T& data):
-            data_{data}
-        {}
+  //! Classe Node
+  /*!
+     Classe nodo, com tipo genérico. Elemento da pilha encadeada simples. Possui
+     um ponteiro que aponta para o próximo. Armazena dado de tipo genérico T.
+  */
+  class Node {
+   public:
+    //! Construtor explícito
+    /*!
+       Construtor explícito.
+       \param data: Referência constante ao dado a ser armazenado (const T&).
+     */
+    explicit Node(const T& data) : data_{data} {}
 
-        Node(const T& data, Node* next):
-            data_{data},
-            next_{next}
-        {}
+    //! Construtor
+    /*!
+        Construtor com parâmetros de dado e próximo.
+        \param data: Referência constante ao dado a ser armazenado (const T&).
+        \param next: Próximo nodo da pilha (Node*).
+     */
+    Node(const T& data, Node* next) : data_{data}, next_{next} {}
 
-        T& data() {  // getter: dado
-            return data_;
-        }
+    //! Método dado
+    /*!
+       Getter do atributo privado data_. Dado armazenado no nodo.
+       \return Referência ao dado armazenado no nodo, de tipo genérico T (T&).
+     */
+    T& data(void) { return data_; }
 
-        const T& data() const {  // getter const: dado
-            return data_;
-        }
+    //! Método dado
+    /*!
+       Getter const do atributo privado data_. Dado armazenado no nodo.
+       \return Referência constante ao dado armazenado no nodo, de tipo genérico
+       T (const T&).
+     */
+    const T& data(void) const { return data_; }
 
-        Node* next() {  // getter: próximo
-            return next_;
-        }
+    //! Método próximo
+    /*!
+        Getter do próximo nodo da cadeia.
+        \return Ponteiro para nodo.
+     */
+    Node* next(void) { return next_; }
 
-        const Node* next() const {  // getter const: próximo
-            return next_;
-        }
+    //! Método próximo
+    /*!
+       Getter const do próximo nodo da cadeia.
+       \return Ponteiro constante para nodo
+     */
+    const Node* next(void) const { return next_; }
 
-        void next(Node* node) {  // setter: próximo
-            next_ = node;
-        }
+    //! Método próximo
+    /*!
+       Setter do atributo next_.
+       \param node: Passagem por referência do nodo para configurar com próximo
+       (Node *).
+     */
+    void next(Node* node) { next_ = node; }
 
-     private:
-        T data_;
-        Node* next_{nullptr};
-    };
+    //! Dado
+    /*!
+      Dado a ser armazenado. Tipo genérico T.
+    */
+    T data_;
 
-    Node* end() {  // último nodo da lista
-        auto it = head;
-        for (auto i = 1u; i < size(); ++i) {
-            it = it->next();
-        }
-        return it;
-    }
+    //! Ponteiro para próximo
+    /*!
+      Ponteiro para o próximo nodo. De tipo ponteiro para Node. Quando é o
+      último nodo da pilha aponta para nullptr.
+    */
+    Node* next_{nullptr};
+  };
 
-     Node* current(std::size_t index) {  // retorna node com idice
-        auto it = head;
-        if (index != 0) {
-            for (auto i = 1; i < size(); ++i) {
-            if (i == index)
-                return it;
-            it = it->next();
-            }
-        }
-        return it;
-    }
+  //! Topo da Pilha
+  /*!
+     Ponteiro para nodo. Aponta para o nodo no topo da pilha.
+   */
+  Node* top_{nullptr};
 
-    Node* head{nullptr};
-    std::size_t size_{0u};
+  //! Tamanho
+  /*!
+     Tamanho da pilha. Tipo size_t.
+   */
+  std::size_t size_{0u};
 };
 
 }  // namespace structures
 
 #endif
-
