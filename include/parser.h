b497ffc550f8cdfc1aef771d29d3bb895b929e4d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-26 18:51:15 -0300

    parser works with string instead of file

--- include/parser.h
+++ include/parser.h
@@ -1,20 +1,21 @@
 #ifndef PARSER_H
-#include "linked_stack.h"
-#include <stdio.h>
+#define PARSER_H
+
 #include <string.h>
-#include <fstream>
+
+#include "linked_stack.h"
 
 class Parser {
-  public:
-    Parser(std::ifstream &file);
-    //! Verify semantics
-    bool parse_file();
-    //! Checks if a opening_tag matches a closing_tag
-    bool match(std::string opening_tag, std::string closing_tag);
+ public:
+  Parser(std::string content);
+  //! Verify semantics
+  bool parse_file();
+  //! Checks if a opening_tag matches a closing_tag
+  bool match(std::string opening_tag, std::string closing_tag);
 
-  private:
-      std::ifstream &file_;
-      structures::LinkedStack<std::string> linked_stack;
+ private:
+  std::string content_;
+  structures::LinkedStack<std::string> linked_stack;
 };
 
 #endif
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
 #include <parser.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+
 #include <fstream>
+#include <iostream>
 #include <regex>
 #include <stdexcept>
 
@@ -10,39 +11,40 @@ static const char LEFT_BRACKET = '<';
 static const char RIGHT_BRACKET = '>';
 static const char SLASH = '/';
 
-Parser::Parser(std::ifstream &file) : file_(file) {}
+Parser::Parser(std::string content) : content_(content) {}
 
 bool Parser::parse_file() {
-  std::string line;
   std::string tag;
   std::size_t tag_begin;
   std::size_t tag_length;
-  while (getline(file_, line)) {
-    for (size_t i = 0; i < line.size(); i++) {
-      if(line[i] == LEFT_BRACKET) {
-        tag_begin = i;
-      }
 
-      if (line[i] == RIGHT_BRACKET) {
-        tag_length = i + 1 - tag_begin;
-        tag = line.substr(tag_begin, tag_length);
+  auto index = 0u;
+  while (index < content_.length()) {
+    if (content_[index] == LEFT_BRACKET) {
+      tag_begin = index;
+    }
 
-        if (tag[1] == SLASH) {
-          std::string last_tag = linked_stack.pop();
-          if(!match(last_tag, tag))
-            return false;
-        } else {
-          linked_stack.push(tag);
+    if (content_[index] == RIGHT_BRACKET) {
+      tag_length = index + 1 - tag_begin;
+      tag = content_.substr(tag_begin, tag_length);
+
+      if (tag[1] == SLASH) {
+        std::string last_tag = linked_stack.pop();
+        if (!match(tag, last_tag)) {
+          return false;
         }
+      } else {
+        linked_stack.push(tag);
       }
     }
+    index++;
   }
 
   return linked_stack.empty();
 }
 
 bool Parser::match(std::string opening_tag, std::string closing_tag) {
-  std::regex reg ("\\W");
+  std::regex reg("\\W");
 
   std::string opening_tag_text = std::regex_replace(opening_tag, reg, "");
   std::string closing_tag_text = std::regex_replace(closing_tag, reg, "");
