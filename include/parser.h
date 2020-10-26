263d923abf4c6c84c8f791537f69352c6eff3fb5
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-26 19:58:04 -0300

    using assembly_tag() to assembly tag on parser_file()

--- include/parser.h
+++ include/parser.h
@@ -12,10 +12,11 @@ class Parser {
   bool parse_file();
   //! Checks if a opening_tag matches a closing_tag
   bool match(std::string opening_tag, std::string closing_tag);
-
  private:
   //! Check if content_at_index is equal to tag_element
   bool isTagElement(const char& content_at_index, const char& tag_element);
+
+  std::string assembly_tag(std::size_t begin, std::size_t index);
 
   std::string content_;
   structures::LinkedStack<std::string> linked_stack;
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,11 +1,11 @@
 #include <parser.h>
 #include <stdio.h>
-#include <string.h>
 
 #include <fstream>
 #include <iostream>
 #include <regex>
 #include <stdexcept>
+#include <string>
 
 static const char LEFT_BRACKET = '<';
 static const char RIGHT_BRACKET = '>';
@@ -13,23 +13,23 @@ static const char SLASH = '/';
 
 Parser::Parser(std::string content) : content_(content) {}
 
-
 bool Parser::parse_file() {
   std::string tag;
   std::size_t tag_begin;
   std::size_t tag_length;
 
   auto index = 0u;
+  char content;
+
   while (index < content_.length()) {
-    char content = content_[index];
+    content = content_[index];
 
     if (isTagElement(content, LEFT_BRACKET)) {
       tag_begin = index;
     }
 
     if (isTagElement(content, RIGHT_BRACKET)) {
-      tag_length = index + 1 - tag_begin;
-      tag = content_.substr(tag_begin, tag_length);
+      tag = assembly_tag(tag_begin, index);
 
       if (isTagElement(tag[1], SLASH)) {
         std::string last_tag = linked_stack.pop();
@@ -57,6 +57,13 @@ bool Parser::match(std::string opening_tag, std::string closing_tag) {
   return match == 0;
 }
 
-bool Parser::isTagElement(const char& content_at_index, const char& tag_element) {
+bool Parser::isTagElement(const char& content_at_index,
+                          const char& tag_element) {
   return content_at_index == tag_element;
+}
+
+std::string Parser::assembly_tag(std::size_t begin, std::size_t index) {
+  std::size_t length;
+  length = index + 1 - begin;
+  return content_.substr(begin, length);
 }
