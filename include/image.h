676221547bb8929422805594e9f9831512a4f8d9
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-29 14:08:50 -0300

    initial commit for image class

---
+++ include/image.h
@@ -0,0 +1,20 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <string>
+
+class Image {
+  public:
+    Image(void);
+  private:
+    // Nome da imagem
+    std::string name_;
+    // Altura da imagem
+    std::size_t height;
+    // Largura da imagem
+    std::size_t width;
+    // Dados da imagem
+    int** matrix;
+}
+
+#endif
