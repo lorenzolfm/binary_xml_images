b48eaf2c0d6d2a97279151035f3c59a8d5cc75c2
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-11-01 16:47:23 -0300

    including linked_queue

--- tests/AllTests.cpp
+++ tests/AllTests.cpp
@@ -6,6 +6,7 @@
 
 #include "gtest/gtest.h"
 #include "linked_stack.h"
+#include "linked_queue.h"
 #include "parser.h"
 
 int main(int argc, char* argv[]) {
@@ -139,4 +140,88 @@ TEST_F(LinkedStackTest, TopAsLhsWorks) {
   fill();
   stack.top() = -2;
   ASSERT_EQ(-2, stack.top());
+}
+
+class LinkedQueueTest : public ::testing::Test {
+ protected:
+  structures::LinkedQueue<int> queue{};
+
+  void fill(void) {
+    for (auto i = 0; i < 10; i++) {
+      queue.enqueue(i);
+    }
+  }
+};
+
+TEST_F(LinkedQueueTest, SizeReturnsCorrectValue) {
+  ASSERT_EQ(0, queue.size());
+
+  fill();
+  ASSERT_EQ(10, queue.size());
+}
+
+TEST_F(LinkedQueueTest, EmptyReturnsTrueWhenEmpty) {
+  ASSERT_TRUE(queue.empty());
+}
+
+TEST_F(LinkedQueueTest, EmptyReturnsFalseWhenNotEmpty) {
+  fill();
+  ASSERT_FALSE(queue.empty());
+}
+
+TEST_F(LinkedQueueTest, EnqueueIncreasesSize) {
+  queue.enqueue(1);
+  ASSERT_EQ(1, queue.size());
+  queue.enqueue(1);
+  ASSERT_EQ(2, queue.size());
+}
+
+TEST_F(LinkedQueueTest, EnqueuesData) {
+  for (auto i = 0; i < 10; i++) {
+    queue.enqueue(i);
+    ASSERT_EQ(i, queue.back());
+  }
+}
+
+TEST_F(LinkedQueueTest, BackReturnCorrectElement) {
+  for (auto i = 0; i < 10; i++) {
+    queue.enqueue(i);
+    ASSERT_EQ(i, queue.back());
+  }
+}
+
+TEST_F(LinkedQueueTest, BackThrowsErrorOnEmptyQueue) {
+  ASSERT_THROW(queue.back(), std::out_of_range);
+}
+
+TEST_F(LinkedQueueTest, BackWorksAsLhs) {
+  fill();
+  queue.back() = -2;
+  ASSERT_EQ(-2, queue.back());
+}
+
+TEST_F(LinkedQueueTest, ClearSetsSize0) {
+  fill();
+  queue.clear();
+  ASSERT_EQ(0, queue.size());
+}
+
+TEST_F(LinkedQueueTest, DequeueDecreasesSize) {
+  fill();
+  for (auto i = 9; i >= 0; i--) {
+    queue.dequeue();
+    ASSERT_EQ(i, queue.size());
+  }
+}
+
+TEST_F(LinkedQueueTest, DequeueThrowsErrorWhenEmpty) {
+  ASSERT_THROW(queue.dequeue(), std::out_of_range);
+}
+
+TEST_F(LinkedQueueTest, DequeueReturnsCorrectElement) {
+  fill();
+
+  for (auto i = 0; i < 10; i++) {
+    ASSERT_EQ(i, queue.dequeue());
+  }
 }
