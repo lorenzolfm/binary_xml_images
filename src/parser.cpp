26771a35c1b831bf1a01b5db6ee4ced7043a0073
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-11-02 10:02:35 -0300

    refactoring parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,7 +1,8 @@
+#include "parser.h"
+
 #include <cstdint>
 #include <regex>
 
-#include "parser.h"
 #include "linked_stack.h"
 
 Parser::Parser(std::string xml_content) : content(xml_content) {}
@@ -49,78 +50,3 @@ bool Parser::match(const std::string& opening_tag,
 
   return match == 0;
 }
-
-// bool Parser::parse_file() {
-// structures::LinkedStack<std::string> linked_stack;
-
-// Image img;
-// std::string tag{""};
-// std::size_t tag_begin{0u};
-// std::size_t left_bracket_of_closing_tag_position{0u};
-// std::size_t right_bracket_of_opening_tag_position{0u};
-// std::size_t index{0u};
-// char content{' '};
-
-// while (index < content_.length()) {
-// content = content_[index];
-
-// if (content == LEFT_BRACKET) {
-// tag_begin = index;
-
-// if (content_[index + 1] == SLASH) {
-// left_bracket_of_closing_tag_position = index;
-//}
-//}
-
-// if (content == RIGHT_BRACKET) {
-// tag = extract_substr(tag_begin, index);
-
-// if (tag[1] == SLASH) {
-// std::string last_tag = linked_stack.pop();
-
-// if (!match(tag, last_tag)) {
-// return false;
-//}
-
-// std::size_t length_of_content = left_bracket_of_closing_tag_position -
-// right_bracket_of_opening_tag_position -
-// 1;
-
-// std::string tag_data = content_.substr(
-// right_bracket_of_opening_tag_position + 1, length_of_content);
-
-////if (last_tag == "<name>") {
-////img.name(tag_data);
-////} else if (last_tag == "<height>") {
-////img.height(std::stoi(tag_data));
-////} else if (last_tag == "<width>") {
-////img.width(std::stoi(tag_data));
-////} else if (last_tag == "<data>") {
-////img.data(tag_data);
-//////std::cout << img.name() << " " << img.label() << std::endl;
-////images_.push_back(img);
-////}
-
-//} else {
-// right_bracket_of_opening_tag_position = index;
-
-// linked_stack.push(tag);
-//}
-//}
-// index++;
-//}
-
-// return linked_stack.empty();
-//}
-
-// std::string Parser::extract_substr(std::size_t begin, std::size_t index) {
-// std::size_t length;
-// length = index + 1 - begin;
-// return content_.substr(begin, length);
-//}
-
-// void Parser::display() {
-////for (auto i = 0; i < images_.size(); i++) {
-////std::cout << images_[i]->name() << " " << images_[i]->label() << std::endl;
-////}
-//}
