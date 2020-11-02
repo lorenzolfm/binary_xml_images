c9c22be1e3de06e05bff8505bd0a6d61e44d543d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-11-02 15:18:01 -0300

    image constructor will use vector of strings from parsed_data

--- include/image.h
+++ include/image.h
@@ -3,9 +3,20 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 class Image {
  public:
+  //! Construtor
+  /*!
+     Constrói uma instância do classe Imagem. Recebe como parâmetros um vetor de
+     strings que contém as todos os elementos do XML relevantes para uma imagem.
+
+     \param tags: Vetor de strings que contém todos os elementos do XML
+     (elemento = tag abertura + conteúdo + tag fechamento) relevantes para o
+     objeto imagem
+   */
+  Image(std::vector<std::string> tags);
 
  private:
   //! Nome.
