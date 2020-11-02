74191c1ce690febde656babc2630fcfb109aa1ca
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-11-01 22:39:50 -0300

    removing unused stuff

--- include/parser.h
+++ include/parser.h
@@ -18,13 +18,9 @@ class Parser {
 
   void display();
  private:
-
   std::string extract_substr(std::size_t begin, std::size_t index);
 
-  void initialize_image_object(std::string tag, std::string tag_content);
-
   std::string content_;
-  structures::LinkedStack<std::string> linked_stack;
   std::vector<Image> images_;
 
   static const char LEFT_BRACKET = '<';
